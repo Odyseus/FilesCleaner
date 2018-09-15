fa5e4764153c6ab24448cd000c0aa6c23bcf7110
Odyseus <venusyadonis@gmail.com>
2018-09-15 01:24:37 -0300

    Bump version.

--- AppData/FilesCleanerApp/__init__.py
+++ AppData/FilesCleanerApp/__init__.py
@@ -4,4 +4,4 @@
 __status__ = "(Stable)"
 __appname__ = "Files Cleaner"
 __appdescription__ = "A CLI utility to delete/clean files from a folder based on a pattern of file names on GNU/Linux."
-__version__ = "2018-06-12 02:20:15.234"
+__version__ = "2018-09-15 01:19:13.335"
