4e2de112f059440e0a213233712127396a81e0bd
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:38:56 -0300

    Added README and updated __init__.py module.

--- AppData/FilesCleanerApp/__init__.py
+++ AppData/FilesCleanerApp/__init__.py
@@ -5,3 +5,7 @@ __status__ = "(Stable)"
 __appname__ = "Files Cleaner"
 __appdescription__ = "A CLI utility to delete/clean files from a folder based on a pattern of file names on GNU/Linux."
 __version__ = "2018-09-15 01:19:13.335"
+
+
+if __name__ == "__main__":
+    pass
