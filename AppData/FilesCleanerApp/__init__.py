86515944bc3cd319bddaee6324d2da4c0d36cac3
Odyseus <venusyadonis@gmail.com>
2020-03-08 09:13:15 -0300

    Updated manual page and bump version

--- AppData/FilesCleanerApp/__init__.py
+++ AppData/FilesCleanerApp/__init__.py
@@ -3,7 +3,7 @@
 __status__ = "Stable"
 __appname__ = "Files Cleaner"
 __appdescription__ = "CLI utility to delete/clean files from a folder based on a pattern of file names on GNU/Linux."
-__version__ = "2020-02-19 12:00:13.464"
+__version__ = "2020-03-08 09:10:52.455"
 
 
 if __name__ == "__main__":
